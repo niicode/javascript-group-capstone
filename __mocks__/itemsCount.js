ded02267486849a08e796633b99e556df10ddd59
niicode <niidarku66@gmail.com>
2022-09-01 17:57:57 +0000

    add test to functions

--- __mocks__/itemsCount.js
+++ __mocks__/itemsCount.js
@@ -22,9 +22,7 @@ export const movies = [
       medium: 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
     },
     summary: 'The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on the family crime syndicate.',
-  }
+  },
 ];
 
-export const itemCount = (movies) => {
-  return movies.length;
-}
+export const itemCount = (movies) => movies.length;
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,23 @@
 import { itemCount, movies } from '../__mocks__/itemsCount.js';
-import { comment} from '../__mocks__/commentCount.js';
 import { commentsCount } from '../src/modules/Comments.js';
+
+const comment = [
+  {
+    item_id: 1,
+    username: 'John Doe',
+    comment: 'This is a comment',
+  },
+  {
+    item_id: 2,
+    username: 'John Doe',
+    comment: 'This is a comment',
+  },
+  {
+    item_id: 3,
+    username: 'John Doe',
+    comment: 'This is a comment',
+  },
+];
 
 describe('count number of items', () => {
   test('count number of items', () => {
